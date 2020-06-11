9ee9cc9745906e233cd7615ec8240c26308ccfde
frax <axtelius@gmail.com>
2020-06-11 08:28:32 +0200

    tmp disable jet framework fix
    Not sure it's needed any more, seems to fxxk up XT.

--- WpCleanUp.php
+++ WpCleanUp.php
@@ -18,8 +18,8 @@ add_action('init', function() {
 });
 
 // This need to run before init...
-add_filter( 'cherry_core_base_url', 'jet_fix_framework_url' );
-add_filter( 'cx_include_module_url', 'jet_fix_framework_url' );
+// add_filter( 'cherry_core_base_url', 'jet_fix_framework_url' );
+// add_filter( 'cx_include_module_url', 'jet_fix_framework_url' );
 
 function jet_fix_framework_url( $url ) {
         return home_url( '/' ) . explode( '/web/', $url )[1];
